correct loss function and attention mask

absnlp/model/bert.py
@@ -17,7 +17,7 @@ class BertNerModule(pl.LightningModule):
         self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.dropout = nn.Dropout(self.encoder_conf.dropout)
         self.linear = nn.Linear(768, self.num_labels)
-        self.loss = nn.CrossEntropyLoss(ignore_index=0)
+        self.loss = nn.CrossEntropyLoss()
         self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, batch):
@@ -45,13 +45,13 @@ class BertNerModule(pl.LightningModule):
     def _shared_step(self, batch: dict, batch_idx: int):
         tag_ids = batch["tag_ids"]
         forward_output = self.forward(batch)
-        loss, f1 = self._evaluate(forward_output, tag_ids)
+        attention_mask=batch['attention_mask']
+        loss, f1 = self._evaluate(forward_output, tag_ids, attention_mask)
         return loss, f1
 
-    def _evaluate(self, logits, labels):
+    def _evaluate(self, logits, labels, mask):
         pred = self.softmax(logits)
         pred = torch.argmax(pred, dim=-1)
-        mask = (labels != 0) & (labels != -100)
         pred_no_pad, labels_no_pad, logits_no_pad = pred[mask], labels[mask], logits[mask]
         f1 = f1_score(pred_no_pad, labels_no_pad, num_classes=self.num_labels, average="macro")
         loss = self.loss(logits_no_pad.view(-1, logits_no_pad.shape[-1]), labels_no_pad.view(-1))

example/bert_with_data_loader.py
@@ -52,9 +52,9 @@ class ExampleDataset(Dataset):
             token_tag_ids = []
             for word_id in word_ids:
                 if word_id is None:
-                    token_tag_ids.append(0)
+                    token_tag_ids.append(-100)
                 elif word_id == previous_word_id:
-                    token_tag_ids.append(-1000)
+                    token_tag_ids.append(-100)
                 else:
                     token_tag_ids.append(tag_ids[word_id])
             self.samples.append({
@@ -91,9 +91,9 @@ if __name__ == "__main__":
         tags = batch['tag_ids']
         mask = (tags != 0) & (tags != -1000)
         logits_no_pad, tags_no_pad = logits[mask], tags[mask]
-        # pred = softmax(logits)
-        # pred = torch.argmax(pred, dim=-1)
-        # pred_no_pad, labels_no_pad = pred[mask], labels[mask]
+        pred = softmax(logits)
+        pred = torch.argmax(pred, dim=-1)
+        pred_no_pad, labels_no_pad = pred[mask], labels[mask]
         # f1 = f1_score(pred_no_pad, labels_no_pad, num_classes=self.num_labels, average="macro")
-        loss = loss_fn(logits_no_pad.view(-1, logits_no_pad.shape[-1]), tags_no_pad.view(-1))
+        loss = loss_fn(logits.view(-1, logits.shape[-1]), tags.view(-1))
         print(loss)