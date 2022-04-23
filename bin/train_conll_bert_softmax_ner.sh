9cf2d7c7fa1dd9fa907f34ddf43c189f8e5cbfda
Fu, Biye <biye.fu@outlook.com>
2022-04-23 22:47:55 +0800

    create running script

---
+++ bin/train_conll_bert_softmax_ner.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+MODEL_TYPE='bert'
+MODEL_NAME_OR_PATH='bert-base-uncased'
+OUTPUT_DIR='output/bert'
+
+python train.py \
+--model_type $MODEL_TYPE \
+--model_name_or_path $MODEL_NAME_OR_PATH \
+--output_dir $OUTPUT_DIR \
+--overwrite_output_dir \
+--do_train \
+--do_eval \
+--evaluate_during_training \
+--adv_training fgm \
+--num_train_epochs 3 \
+--max_seq_length 128 \
+--logging_steps 0.2 \
+--batch_size 16 \
+--learning_rate 5e-5 \
+--bert_lr 5e-5 \
+--classifier_lr  5e-5 \
